cbf4ee8db311b9eb3ec1cebee64ecc612c2fab90
Castiel <castielwongsysu@gmail.com>
2017-07-30 07:43:03 +0930

    Update gitignore, controller.js and delete all unnecessary data

--- javascript/controller.js
+++ javascript/controller.js
@@ -21,3 +21,38 @@ function addMarkers(map) {
 		map: map
 	})
 }
+
+function getData() {
+	var mydata = JSON.parse("test.json");
+	// console.log(mydata);
+	alert(mydata[0].name);
+}
+
+function print(){
+	alert("aaaaa");
+}
+
+
+function loadJSON(file, callback) {   
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', file, true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+ 
+
+function load(json) {
+    loadJSON(json, function(response) {
+        var actual_JSON = JSON.parse(response);
+        console.log(actual_JSON);
+    });
+    
+    
+}
